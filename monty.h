c8025eef4b5f5e1c79a50772f6b28116d1a0ac43
BENSON <105021700+Muigai-ben@users.noreply.github.com>
2023-09-20 23:15:51 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -13,7 +13,7 @@
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
- * @prev: points to the previous element of the stack (or queue)
+ * @prev: points to the previous element of the stack (or queue).
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
